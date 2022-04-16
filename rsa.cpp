b8c7a175409a2f58088ecfd496dfa944b0b98f73
154912369 <47050695+154912369@users.noreply.github.com>
2022-04-17 01:36:19 +0800

    Update rsa.cpp

--- rsa.cpp
+++ rsa.cpp
@@ -9,6 +9,32 @@ void print(unsigned char* a){
     std::cout << std::endl;
 }
 
+bool compare(const unsigned char* data, const unsigned char* bytes){
+    for (int i = BYTESIZE-1; i >= 0; i--){
+        if(data[i]>bytes[i]){
+            return true;
+        }else if(data[i]<bytes[i]){
+            return false;
+        }
+    }
+    return false;
+}
+bool equal(const unsigned char* data, const unsigned char* bytes){
+    for (int i = 0; i < BYTESIZE; i++){
+        if(data[i]!=bytes[i]){
+            return false;
+        }
+    }
+    return true;
+}
+bool equal_zero(const unsigned char* data){
+    for (int i = 0; i < BYTESIZE; i++){
+        if(data[i]!=0){
+            return false;
+        }
+    }
+    return true;
+}
 bool char_self_add(unsigned char& a, unsigned char b, bool add){
     bool overflow = false;
     unsigned char tmp = 0;
@@ -50,13 +76,84 @@ bool minus_bit(unsigned char* a,unsigned char *b){
     }
     return add;
 }
+int log2(unsigned char* a){
+     int size = 0;
+    int start = BYTESIZE*8-1;
+    int first =0;
+    int second =0;
+    while (size==0&&start>0){
+        first=start&7;
+        second = start>>3;
+        if(a[second]&(1<<first)){
+            return start;
+        }
+        start-=1;
+    }
+    return -1;
+}
+void left_bit(unsigned char* a,int size){
+    int pre_fist=(BYTESIZE-1-(size>>3));
+    int pre_bias =(size&7);
+    unsigned char pre=0;
+    for(int i=BYTESIZE-1;i>=0;i--){
+        if(pre_fist<0){
+            a[i]=0;
+        }else if(pre_fist==0){
+            a[i]=(a[pre_fist]<<pre_bias);
+        }else{
+            a[i]=(a[pre_fist]<<pre_bias)+(a[pre_fist-1]>>(8-pre_bias));
+        }
+        pre_fist-=1;
+    }
+}
+void right_bit(unsigned char* a,int size){
+    int pre_fist=(size>>3);
+    int pre_bias =(size&7);
+    unsigned char pre=0;
+    for(int i=0;i<BYTESIZE;i++){
+        if(pre_fist>BYTESIZE){
+            a[i]=0;
+        }else if(pre_fist==BYTESIZE-1){
+            a[i]=(a[pre_fist]>>(pre_bias));
+        }else{
+            a[i]=(a[pre_fist]>>(pre_bias))+(a[pre_fist+1]<<(8-pre_bias));
+        }
+        pre_fist+=1;
+    }
+}
 
-bool multiply_bit(unsigned char* a,unsigned char *b, unsigned char*ans){
+bool divide_bit(unsigned char* a,unsigned char *b,unsigned char *redis){
+    unsigned char ans[BYTESIZE] ={0};
+    unsigned char bias[BYTESIZE];
+    for(int i=0;i<BYTESIZE;i++){
+        bias[i]=b[i];
+    }
+    int size = log2(a)-log2(b);
+    left_bit(bias,size);
+
+    while(!compare(b,a)){
+        
+        if(!compare(bias,a)){
+            ans[size>>3]+=(1<<(size&7));
+            minus_bit(a,bias);
+        }
+        right_bit(bias,1);
+        size-=1;
+    }
+    for(int i=0;i<BYTESIZE;i++){
+       redis[i]=a[i];
+       a[i]=ans[i];
+    }
+    
+}
+
+bool 
+multiply_bit(unsigned char* a,unsigned char *b, unsigned char*ans){
     bool a_flat[BYTESIZE*8];
     bool b_flat[BYTESIZE*8];
     for(int i=0;i<BYTESIZE*8;i++){
-        int first =i/8;
-        int second =(1<<(i%8));
+        int first =i>>3;
+        int second =(1<<(i&7));
         if(a[first]&second){
             a_flat[i]=true;
         }else{
@@ -105,32 +202,101 @@ bool multiply_bit(unsigned char* a,unsigned char *b, unsigned char*ans){
     
     return false;
 }
-
-bool compare(const unsigned char* data, const unsigned char* bytes){
-    for (int i = BYTESIZE-1; i >= 0; i--){
-        if(data[i]>bytes[i]){
-            return true;
-        }else if(data[i]<bytes[i]){
+bool ex_gcd(unsigned char* a, unsigned char* b, unsigned char* x, unsigned char* y, bool& x_flag, bool& y_flag){
+    if(equal_zero(b)){
+        for(int i=0;i<BYTESIZE;i++){
+            if(i==0){
+                if(a[i]!=1){
+                    return false; 
+                }
+                x[i]=1;
+                y[i]=0;
+            }else{
+                if(a[i]!=0){
+                    return false; 
+                }
+                x[i]=0;
+                y[i]=0;
+            }
+        }
+        x_flag=true;
+        y_flag =true;
+        return true;
+    }else{
+        unsigned char next_a[BYTESIZE];
+        unsigned char next_b[BYTESIZE];
+        unsigned char resi[BYTESIZE];
+        for(int i=0;i<BYTESIZE;i++){
+            next_a[i]=a[i];
+            next_b[i]=b[i];
+        }
+        divide_bit(next_a,next_b,resi);        
+        if(!ex_gcd(next_b,resi,y,x,y_flag,x_flag)){
             return false;
         }
+        for(int i=0;i<BYTESIZE;i++){
+            next_b[i]=0;
+        }
+        multiply_bit(next_a,x,next_b);
+        if(y_flag&&x_flag){
+            if(!compare(next_b,y)){
+                minus_bit(y,next_b);
+            }else{
+                minus_bit(next_b,y);
+                for(int i=0;i<BYTESIZE;i++){
+                    y[i]=next_b[i];
+                }
+                y_flag=false;
+            }
+        }else if(y_flag&&!x_flag){
+             if(add_bit(y,next_b)){
+                 return false;
+             }
+        }else if(x_flag&&!y_flag){
+             if(add_bit(y,next_b)){
+                 return false;
+             }
+        }else{
+            if(!compare(next_b,y)){
+                minus_bit(y,next_b);
+            }else{
+                minus_bit(next_b,y);
+                for(int i=0;i<BYTESIZE;i++){
+                    y[i]=next_b[i];
+                }
+                y_flag=true;
+            }
+        }
+        return true;
     }
-    return false;
 }
-bool equal(const unsigned char* data, const unsigned char* bytes){
-    for (int i = 0; i < BYTESIZE; i++){
-        if(data[i]!=bytes[i]){
-            return false;
+
+bool inv(unsigned char* t, unsigned char* p, unsigned char* ans){
+    unsigned char x[BYTESIZE];
+    bool x_flag;
+    bool y_flag;
+    if(!ex_gcd(t,p,ans,x,x_flag,y_flag)){
+        return false;
+    };
+    if(!x_flag){
+        divide_bit(ans,p,x);
+        for(int i=0;i<BYTESIZE;i++){
+            ans[i]=p[i];
         }
+        minus_bit(ans,x);
     }
+
     return true;
 }
+
+
 bool int2bi(int a, unsigned char* result){
     int i = 0;
     while (i<BYTESIZE*8 &&  a)
     {
-        int first = i / 8;
-        int second = i % 8;
-        if(a%2 == 1){
+        int first = (i>>3);
+        int second = (i&7);
+        if((a&1) == 1){
             result[first] |= (1 << second);
         }
         i += 1;
@@ -179,7 +345,7 @@ int char2str(unsigned char *encode_text, int size, char* result){
         int j = 0;
         while (index)
         {
-            answer = index % 16;
+            answer = (index & 15);
             if (answer <= 9)
             {
                 result[i * 8 / 4 + j] = '0'+answer;
@@ -188,7 +354,7 @@ int char2str(unsigned char *encode_text, int size, char* result){
             {
                 result[i * 8 / 4 + j] = 'a'+(answer-10);
             }
-            index = index / 16;
+            index >>=4;
             j += 1;
         }
         for (; j < 8 / 4;j++){
@@ -211,7 +377,6 @@ public:
         for (int i = 0; i<BYTESIZE;i++){
             _data[i] = bytes[i];
         }
-        int value= 1;
         unsigned char tmp_base[BYTESIZE] = {0};
         unsigned char bias[BYTESIZE] = {0};
         bias[0] = 1;
@@ -230,7 +395,6 @@ public:
                     }
                 }
 
-                value = (value * 2) % 2123;
                 bool add = add_bit(tmp_base, tmp_base);
                 while (add){
                     add = add_bit(tmp_base, _base + (BYTESIZE * 8) * BYTESIZE);
@@ -257,6 +421,11 @@ public:
                     get_residual(tmp_base, tmp_base);
                 }
             }
+        }
+    }
+    void print_bit(){
+        for(int i=0;i<BYTESIZE*2*8;i++){
+            print(_base+i*BYTESIZE);
         }
     }
 
@@ -312,10 +481,11 @@ public:
         int second = 0;
         bool add = false;
         ans[0] = 1;
-        while (index < BYTESIZE)
+        int size = log2(exp)+1;
+        while (index < size)
         {
-            first = index / 8;
-            second = index % 8;
+            first = (index>>3);
+            second = (index&7);
             if(exp[first]&1<<second){
                 multiply(ans, tmp_ans, ans);
             }
@@ -324,24 +494,41 @@ public:
         }
         get_residual(ans, ans);
     }
+    
+    void  multiply(unsigned char* a,unsigned char *b, unsigned char*ans){
+        bool a_flat[BYTESIZE*8];
+        bool b_flat[BYTESIZE*8];
+        for(int i=0;i<BYTESIZE*8;i++){
+            int first =(i>>3);
+            int second =(1<<(i&7));
+            if(a[first]&second){
+                a_flat[i]=true;
+            }else{
+                a_flat[i]=false;
+            }
+            if(b[first]&second){
+                b_flat[i]=true;
+            }else{
+                b_flat[i]=false;
+            }
 
-    void multiply(unsigned char* a,  unsigned char* b,  unsigned char* ans){
+        }
+        int number = 0;
         unsigned char tmp_ans[BYTESIZE] = {0};
-        for (int i = 0; i < BYTESIZE;i++){
-            for (int j = 0; j < 8;j++){
-                if(a[i]&(1<<j)){
-                    for (int s = 0; s < BYTESIZE; s++){
-                        for (int t = 0; t < 8;t++){
-                            if(b[s]&(1<<t)){
-                                bool add = add_bit(tmp_ans, _base + (i * 8 + j + s * 8 + t) * BYTESIZE);
-                                while(add){
-                                    add = add_bit(tmp_ans, _base + 8*BYTESIZE * BYTESIZE);
-                                }
-                            }
+        while(number<2*BYTESIZE*8){
+            for(int i=0;i<number+1;i++){
+                if(i<BYTESIZE*8&&number-i<BYTESIZE*8){
+                    if(a_flat[i]&&b_flat[number-i]){
+                        bool add = add_bit(tmp_ans, _base+number*BYTESIZE);
+                        while(add){
+                            add = add_bit(tmp_ans, _base + 8*BYTESIZE * BYTESIZE);
                         }
                     }
                 }
+                
             }
+            number+=1;
+
         }
         get_residual(tmp_ans, ans);
     }
@@ -353,40 +540,35 @@ class Encrpyt{
         unsigned char _value_e[BYTESIZE]={0};
         unsigned char _key_d[BYTESIZE]={0};
         int _size;
+        bool _ok;
 
     public:
         Encrpyt(int p, int q, int e){
             unsigned char tmp[BYTESIZE] = {0};
-            int2bi(p * q, tmp);
+            unsigned char tmp2[BYTESIZE] = {0};
+            unsigned char tmp3[BYTESIZE] = {0};
+            int2bi(p,tmp2);
+            int2bi(q,tmp3);
+            multiply_bit(tmp2,tmp3,tmp);
             _base_n = BytesBase(tmp);
             for (int i = 0;i<BYTESIZE; i++){
                 if((tmp[i]&((8<<1)-1))){
                     _size = i;
                 }
             }
-                int2bi(e, _value_e);
-
-
+            unsigned char bias[BYTESIZE]={0};
+            bias[0]=1;
+            minus_bit(tmp2,bias);
+            minus_bit(tmp3,bias);
+            int2bi(e, _value_e);
             unsigned char tmp1[BYTESIZE] = {0};
-            int2bi((p-1) * (q-1), tmp1);
-            BytesBase euler_base = BytesBase(tmp1);
-
-            unsigned char bias[BYTESIZE] = {0};
-            unsigned char tmp2[BYTESIZE] = {0};
-            bias[0] = 1;
-            bool add = true;
-            while (compare(tmp1, _key_d)&& add)
-            {
-                euler_base.get_residual(tmp2, tmp2);
-                if (equal(tmp2, bias)){
-                    add = false;
-                }else{
-                    add_bit(_key_d, bias);
-                    add_bit(tmp2, _value_e);
-                }
-                
+             multiply_bit(tmp2,tmp3,tmp1);
+            _ok=inv(_value_e,tmp1,_key_d);
+            if(!_ok){
+                std::cout<<"init failed"<<std::endl;
             }
         }
+        
         void encrp_data(unsigned char* text, unsigned char* result){
             _base_n.exp(text, _value_e, result);
         }
@@ -483,9 +665,8 @@ class Encrpyt{
 
 
 int main(){
-
     std::string a = "你好,你叫什么名字";
-    Encrpyt enc(647,653,659);
+    Encrpyt enc(500881,500887,500891);
     int encode_size = ((a.size() - 1) / enc.get_size() + 1)*(enc.get_size()+1)*2+1;
     char result[encode_size];
     enc.encrp(a.c_str(), result );
